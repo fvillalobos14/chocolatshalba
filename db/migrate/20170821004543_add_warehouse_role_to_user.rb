3de8b41ece9b64dd368c0cfe1b293ba84b4b3e20
René <rene.d.rendon@unitec.edu>
2017-08-20 19:48:47 -0600

    Created dashboard and added warehouse role to user

---
+++ db/migrate/20170821004543_add_warehouse_role_to_user.rb
@@ -0,0 +1,10 @@
+class AddWarehouseRoleToUser < ActiveRecord::Migration[5.1]
+  def change
+  	add_column :users, :warehouse_role, :boolean, default: false
+  	User.create! do |u|
+        u.email     = 'warehouse'
+        u.password    = 'warehouse'
+        u.warehouse_role = true
+	end
+  end
+end
