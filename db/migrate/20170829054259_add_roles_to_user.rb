c1a0a8336b42f0593a96766271a4260be6f87c0f
René <rene.d.rendon@unitec.edu>
2017-08-30 22:51:54 -0600

    Modified seed file

--- db/migrate/20170829054259_add_roles_to_user.rb
+++ db/migrate/20170829054259_add_roles_to_user.rb
@@ -4,30 +4,5 @@ class AddRolesToUser < ActiveRecord::Migration[5.1]
   	add_column :users, :certification_role, :boolean, default: false
   	add_column :users, :quality_role, :boolean, default: false
   	add_column :users, :administration_role, :boolean, default: false
-
-  	User.create! do |u|
-        u.email     = 'qualitymanager'
-        u.password    = 'qualitymanager'
-        u.quality_manager_role = true
-	end
-
-	User.create! do |u|
-        u.email     = 'certification'
-        u.password    = 'certification'
-        u.certification_role = true
-	end
-
-	User.create! do |u|
-        u.email     = 'quality'
-        u.password    = 'quality'
-        u.quality_role = true
-	end
-
-
-	User.create! do |u|
-        u.email     = 'administration'
-        u.password    = 'administration'
-        u.administration_role = true
-	end
   end
 end
