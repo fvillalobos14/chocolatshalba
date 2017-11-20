a16c2c485bc20e87bd19e5e892c7efb3dc715ed9
unknown <rene.d.rendon@unitec.edu>
2017-11-20 11:56:05 -0600

    Added invoice controller and views

---
+++ app/controllers/invoices_controller.rb
@@ -0,0 +1,12 @@
+class InvoicesController < ApplicationController
+    def index
+        @invoices = Invoice.where(paid: false)
+    end
+    
+    def update
+        invoice = Invoice.find(params[:id])
+        if invoice.update(paid: true)
+            redirect_to invoices_path
+        end
+    end
+end
