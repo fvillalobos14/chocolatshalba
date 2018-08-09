29f98631bb9d52b38e4786d2dd741f4f10877710
LourdesZamora <lourdes.zamora@unitec.edu>
2018-08-09 10:13:18 -0600

    Update batches code

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -16,8 +16,32 @@ class Batch < ApplicationRecord
 	validates :entry_control, presence: true
 
 	before_destroy :remove_quality_control
+    before_update :generateCode
+    before_save :generateCode
 
   def remove_quality_control
     QualityControl.where(batch_id: id).destroy_all
   end
+  
+  def generateCode
+    
+    quality=CertificateType.find(self.certificatetype).name
+    cocoat=CocoaType.find(self.cocoaType).name
+    c = entry_control.organization.code+"0"+postharvestCenter.to_s+"-"+entry_control.entryDate.strftime('%d%m%y')+"-"+quality+cocoat+GeneticMaterial.find(self.geneticMaterial).name
+    code=""
+    if ft
+        if quality== 'C' && cocoat == 'C'
+            code=c
+            self.ft=false
+        else  
+            code=c+"-FT"
+
+        end
+    else
+    code= c
+    end
+    self.enterCode=code
+  end
+  
+  
 end
