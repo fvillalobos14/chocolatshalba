575bf491ac1c70f997b5c48953f408c3fb878908
intiv <intiv@unitec.edu>
2018-10-30 16:21:52 -0600

    Numero de hoja se incrementa con cada analisis y se resetea a 1 cada dia

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -6,8 +6,10 @@ def create
 
     @sequence = Sequence.new
     @sequence.year=Time.current.year
+    @sequence.day=Time.current.day
     @sequence.number=1
-    if !Sequence.exists?(year: Time.current.year) 
+    if !Sequence.exists?(year: Time.current.year, day: Time.current.day) 
+        Sequence.delete_all
         if !@sequence.save 
             raise "error"
         end
