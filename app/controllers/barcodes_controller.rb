376c544ee4f3e21de5f0ab7601cf0122fb3ca18c
intiv <intiv@unitec.edu>
2018-11-13 21:43:44 -0600

    Barcode enviado como header de manera dinamica

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -5,10 +5,16 @@ class BarcodesController < ApplicationController
         require 'barby/barcode/code_128'
         require 'barby/outputter/png_outputter'
         @batch=Batch.find(params[:batch_id])
-        bc39 = Barby::Code128B.new(@batch.enterCode)
-        @image = bc39.to_png
-        puts Rails.application.assets.paths[1]
-        File.open(Rails.application.assets.paths[1]+'/barcode.png', 'wb'){ |f| f.write @image}
+        @bc128 = Barby::Code128B.new(@batch.enterCode)
+        @bcimage = Barby::PngOutputter.new(@bc128).to_png
+        respond_to do |format|
+            format.html
+            format.png do
+              send_data @bcimage, type: "image/png", disposition: "inline"
+            end
+          end
+        /puts Rails.application.assets.paths[1]/
+        File.open('barcode.png', 'wb'){ |f| f.write @bcimage}
     end
 
     # def show
